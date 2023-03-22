fb6ef796f03017ec812b65b2d0a52e8f90960aa0
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-03-22 18:09:49 +0100

    start bonus 2, create layout

--- README.md
+++ README.md
@@ -21,12 +21,10 @@
 
 Modificare la classe Movie in modo che accetti piú di un genere.
 
-<!--
-
 ## Bonus 2:
 
 - Creare un layout completo per stampare a schermo una lista di movies.
-- Suddividere il codice in appositi file e cartelle:
-  - creando un file dedicato ai dati che potremmo chiamare db.php
-  - mettendo ciascuna classe nel proprio file e magari raggruppare tutte le classi in una cartella dedicata che possiamo chiamare Models
-  - organizzando il layout dividendo la struttura ed i contenuti in file e parziali dedicati. -->
+  <!-- - Suddividere il codice in appositi file e cartelle: -->
+    <!-- - creando un file dedicato ai dati che potremmo chiamare db.php -->
+    <!-- - mettendo ciascuna classe nel proprio file e magari raggruppare tutte le classi in una cartella dedicata che possiamo chiamare Models -->
+    <!-- - organizzando il layout dividendo la struttura ed i contenuti in file e parziali dedicati. -->
--- index.php
+++ index.php
@@ -4,19 +4,25 @@
         public $release_date;
         public $duration;
         public $genres;
+        public $plot;
 
-        public function __construct(string $_name, int $_release_date, int $_duration, Genre $_genres)
+        public function __construct(string $_name, int $_release_date, int $_duration, Genre $_genres, string $_plot = "unknown")
         {
             $this -> name = $_name;
             $this -> setReleaseDate($_release_date);
             $this -> duration = $_duration;
             $this -> genres = $_genres;
+            $this -> plot = $_plot;
         }
 
         public function setReleaseDate($date) {
             if (!is_numeric($date)) return false;
 
             $this -> release_date = $date;
+        }
+
+        public function getAbstract($limit = 80) {
+            return substr($this -> plot, 0, $limit) . "...";
         }
     }
 
@@ -31,9 +37,14 @@
 
     $genres_1 = new Genre(["Azione", "Avventura"]);
 
+    $plot_1 = "Lo scienziato Bruce Banner ha qualche problema di controllo dell'ira. Nel corso di un esperimento
+    andato male l'uomo si trasforma in un mostro verde che usa la rabbia per salvare le vite umane.";
+    $plot_2 = "Il morso di un ragno mutante dona degli incredibili, e inaspettati, poteri al giovane Peter Parker, che li usa nella guerra contro un minaccioso nemico.
+    ";
 
-    $movie_1 = new Movie("Hulk", 2003, 138, $genres_1);
-    $movie_2 = new Movie("Spider-Man", 2002, 121, $genres_1);
+
+    $movie_1 = new Movie("Hulk", 2003, 138, $genres_1, $plot_1);
+    $movie_2 = new Movie("Spider-Man", 2002, 121, $genres_1, $plot_2);
 ?>
 
 <!DOCTYPE html>
@@ -44,75 +55,83 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Primo Esercizio OOP</title>
+
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <!-- STYLE -->
+    <link rel="stylesheet" href="./css/style.css">
+
 </head>
 
 <body>
-    <style>
-    ul {
-        list-style: none;
-        margin: 1rem;
-    }
-    </style>
+    <div class="container py-5">
+        <h1 class="mb-5">Film</h1>
 
-    <h1>Film</h1>
+        <ul>
+            <li>
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <?= $movie_1 -> name ?>
+                        </h5>
+                        <h6 class="card-subtitle mb-2">
+                            Anno d'uscita:
+                            <?= $movie_1 -> release_date ?>
+                        </h6>
+                        <h6 class="card-subtitle mb-2">
+                            Durata:
+                            <?= $movie_1 -> duration ?>
+                            m
+                        </h6>
+                        <p class="card-text">
+                            <?= $movie_1 -> getAbstract() ?>
+                        </p>
+                        <h6 class="card-subtitle mb-4">
+                            Genere principale:
+                        </h6>
+                        <span class="h6 text-muted d-block">
+                            <?= $movie_1 -> genres -> genres[0]  ?>
+                        </span>
+                    </div>
+                </div>
+            </li>
 
-    <ul>
-        <li>
-            <ul>
-                <li>
-                    Nome:
-                    <?= $movie_1 -> name ?>
-                </li>
+            <li>
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <?= $movie_2 -> name ?>
+                        </h5>
+                        <h6 class="card-subtitle mb-2">
+                            Anno d'uscita:
+                            <?= $movie_2 -> release_date ?>
+                        </h6>
+                        <h6 class="card-subtitle mb-2">
+                            Durata:
+                            <?= $movie_2 -> duration ?>
+                            m
+                        </h6>
+                        <p class="card-text">
+                            <?= $movie_2 -> getAbstract() ?>
+                        </p>
+                        <h6 class="card-subtitle mb-2">
+                            Genere principale:
+                            <ul class="ps-0">
+                                <?php foreach($genres_1 ->genres as $genre) { ?>
+                                <li class="text-muted ms-0 d-block">
+                                    <?= $genre ?>
+                                </li>
+                                <?php } ?>
+                            </ul>
+                        </h6>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
 
-                <li>
-                    Anno d'uscita:
-                    <?= $movie_1 -> release_date ?>
-                </li>
-
-                <li>
-                    Durata:
-                    <?= $movie_1 -> duration ?>
-                    m
-                </li>
-
-                <li>
-                    Genere principale:
-                    <?= $movie_2 -> genres -> genres[0]  ?>
-                </li>
-            </ul>
-        </li>
-
-        <li>
-            <ul>
-                <li>
-                    Nome:
-                    <?= $movie_2 -> name ?>
-                </li>
-
-                <li>
-                    Anno d'uscita:
-                    <?= $movie_2 -> release_date ?>
-                </li>
-
-                <li>
-                    Durata:
-                    <?= $movie_2 -> duration ?>
-                    m
-                </li>
-
-                <li>
-                    Genere principale:
-                    <ul>
-                        <?php foreach($genres_1 ->genres as $genre) { ?>
-                        <li>
-                            <?= $genre ?>
-                        </li>
-                        <?php } ?>
-                    </ul>
-                </li>
-            </ul>
-        </li>
-    </ul>
 
 
 </body>
